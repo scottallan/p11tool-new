e75fd0f5e6803f0c549ba6f5e506ffa545274e49
ScottAllan <scott.allan@securekey.com>
2019-02-24 14:46:32 -0500

    refactored asymmetric unwrap 1

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func main() {
 	pkcs11Library := flag.String("lib", "", "Location of pkcs11 library")
 	slotLabel := flag.String("slot", "ForFabric", "Slot Label")
 	slotPin := flag.String("pin", "98765432", "Slot PIN")
-	action := flag.String("action", "list", "list,import,generate,generateAndImport,generateSecret,generateAES,generateDES,unwrapECWithDES3,unwrapRSAWithDES3,wrapKeyWithDES3,getSKI,getSkiFromCert,getSkiFromB64Cert,SignHMAC384,TestAESGCM,generateCSR,importCert,deleteObj")
+	action := flag.String("action", "list", "list,import,generate,generateAndImport,generateSecret,generateAES,generateDES,wrapKeyWithDES3,unwrapASYMWithDES3,getSKI,getSkiFromCert,getSkiFromB64Cert,SignHMAC384,TestAESGCM,generateCSR,importCert,deleteObj")
 	keyFile := flag.String("keyFile", "/some/dir/key.pem)", "path to key you want to import or getSKI")
 	keyType := flag.String("keyType", "EC", "Type of key (EC,RSA,GENERIC_SECRET,AES,SHA256_HMAC,SHA384_HMAC,DES3)")
 	keyLen := flag.Int("keyLen", 32, "Key Length for CKK_GENERIC_SECRET (32,48,...)")
@@ -277,7 +277,7 @@ func main() {
 		exitWhenError(err)
 		fmt.Printf("successfully tested CKM_SHA384_HMAC on key with LABEL: %s\n HMAC %x\n", *keyLabel, hmac)
 
-	case "unwrapECWithDES3":
+	case "unwrapASYMWithDES3":
 		w, _, err := p11w.FindObjects([]*pkcs11.Attribute{
 			pkcs11.NewAttribute(pkcs11.CKA_LABEL, *wrapKey),
 		},
@@ -285,20 +285,11 @@ func main() {
 		)
 		exitWhenError(err)
 
-		if *keyType == "EC" {
+		switch *keyType {
+		case "EC":
 			err := p11w.UnWrapECKeyFromFile(*keyFile, *keyStore, *keyStorepass, *keyLabel, w[0])
 			exitWhenError(err)
-		}
-
-	case "unwrapRSAWithDES3":
-		w, _, err := p11w.FindObjects([]*pkcs11.Attribute{
-			pkcs11.NewAttribute(pkcs11.CKA_LABEL, *wrapKey),
-		},
-		1,
-		)
-		exitWhenError(err)
-
-		if *keyType == "RSA" {
+		case "RSA":
 			err := p11w.UnWrapRSAKeyFromFile(*keyFile, *keyStore, *keyStorepass, *keyLabel, w[0])
 			exitWhenError(err)
 		}
